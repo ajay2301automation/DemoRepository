372860bac35f997b608815b4df14aa3acfa216e8
Bhardwaj <ajay.bhardwaj-extern@dbschenker.com>
2019-02-10 15:16:48 +0530

    firt git commit for my selenium code

---
+++ src/main/java/com/qa/pages/Search.java
@@ -0,0 +1,9 @@
+package com.qa.pages;
+
+public class Search {
+
+	public void search()
+	{
+		System.out.println("Search page method");
+	}
+}
---
+++ src/main/java/com/qa/util/Login.java
@@ -0,0 +1,9 @@
+package com.qa.util;
+
+public class Login {
+
+	public void loginPage()
+	{
+		System.out.println("LoginPageUTIL");
+	}
+}
